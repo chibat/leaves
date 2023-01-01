e4c50e845c7f1d502b23d0658571a8deb1892e3f
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2023-01-02 00:02:07 +0900

    fix

---
+++ components/Users.tsx
@@ -0,0 +1,18 @@
+
+type Props = {
+  users: { id: number; name: string; picture: string }[];
+};
+
+export default function Users(props: Props) {
+
+  return (
+    <>
+      {props.users.map(user =>
+        <div className="mb-3" key={user.id}>
+          <img src={user.picture} alt="mdo" width="32" height="32" className="rounded-circle me-2" />
+          <a href={`/users/${user.id}`} className="noDecoration">{user.name}</a>
+        </div>
+      )}
+    </>
+  );
+}
