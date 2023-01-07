b8e1e7a2f138079e56f779fb88527864216b2c09
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2023-01-07 13:08:42 +0900

    fix

--- generate_key.ts
+++
@@ -1,8 +0,0 @@
-const key = await crypto.subtle.generateKey(
-  { name: "HMAC", hash: "SHA-512" },
-  true,
-  ["sign", "verify"],
-);
-const jwk = await crypto.subtle.exportKey("jwk", key);
-const jwkString = JSON.stringify(jwk);
-console.log(jwkString);
--- lib/jwt_test.ts
+++
@@ -1,7 +0,0 @@
-import { serializeJwt, deserializeJwt } from "~/lib/jwt.ts";
-
-const jwt = await serializeJwt({ foo: "bar" });
-console.log(jwt);
-
-const payload = await deserializeJwt(jwt);
-console.log(payload);
