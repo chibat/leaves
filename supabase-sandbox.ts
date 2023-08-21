e62168a4ca8d69d4ddeba3a839935d766736650c
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2023-08-22 06:04:02 +0900

    fix

--- supabase-sandbox.ts
+++ supabase-sandbox.ts
@@ -20,3 +20,39 @@ console.log(JSON.stringify(error));
 console.log(JSON.stringify(data));
 
 // 日本語全文検索するためには、DB FUNCTION を作る必要がありそう。
+// 複雑なクエリを実行するためには、function や view を作る必要がありそう。
+
+export async function selectUserByGoogleId(
+  googleId: string,
+) {
+  return await supabase.from("app_user")
+    .select() // TODO
+    .eq(
+      "google_id",
+      googleId,
+    ).maybeSingle();
+}
+
+export async function selectUser(
+  userId: number,
+) {
+  return await supabase.from("app_user")
+    .select() // TODO
+    .eq(
+      "id",
+      userId,
+    ).maybeSingle();
+}
+
+export async function selectUsers() {
+  const results = await supabase.from("post")
+    .select("user_id,max(updated_at)") // TODO
+    .order("user_id");
+
+  return results.data;
+
+  //SELECT user_id, max(updated_at) as updated_at FROM post GROUP BY user_id ORDER BY user_id
+  //return result.rows;
+}
+
+console.log(await selectUsers());
