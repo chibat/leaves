e5eb7041d76375011584d4fa0a3ac5bf11eca3f5
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2023-10-13 07:51:29 +0900

    fix

--- islands/PostEdit.tsx
+++ islands/PostEdit.tsx
@@ -121,6 +121,9 @@ export default function Edit(props: { post: Post }) {
           >
             Draft
           </label>
+          <button class="btn btn-light me-2" onClick={() => location.href = `/posts/${postId}`}>
+            Cancel
+          </button>
           <button
             class="btn btn-primary"
             onClick={save}
