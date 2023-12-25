422e1756b6ea25a62fb06793ac09472adfb96cdd
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2023-12-26 07:52:32 +0900

    fix

--- islands/Header.tsx
+++ islands/Header.tsx
@@ -76,6 +76,21 @@ export default function Header(
             <li class="nav-item">
               <a
                 class="noDecoration nav-link"
+                href="/shortcuts"
+                title="Keyboard Shortcuts"
+              >
+                <img
+                  src="/assets/img/keyboard-fill.svg"
+                  alt="Keyboard Shortcuts"
+                  width="20"
+                  height="20"
+                >
+                </img>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="noDecoration nav-link"
                 href="/posts/new"
                 title="New Post"
               >
@@ -89,21 +104,6 @@ export default function Header(
                 >
                   <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM8.5 7v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 1 0z" />
                 </svg>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a
-                class="noDecoration nav-link"
-                href="/shortcuts"
-                title="Keyboard Shortcuts"
-              >
-                <img
-                  src="/assets/img/keyboard-fill.svg"
-                  alt="Keyboard Shortcuts"
-                  width="20"
-                  height="20"
-                >
-                </img>
               </a>
             </li>
             {props.user &&
