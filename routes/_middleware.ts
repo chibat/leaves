04e8681edafcfa6f49e6a27077a8c80b54de9de2
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2024-01-14 09:58:55 +0900

    fix

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -15,7 +15,8 @@ export async function handler(
   }
   const url = new URL(_req.url);
   if (url.hostname === "leaves.deno.dev") {
-    return Response.redirect("https://leaves.chiba.dev", 301); // Moved Permanently
+    url.hostname = "leaves.chiba.dev";
+    return Response.redirect(url, 301); // Moved Permanently
   }
   if (url.pathname === "/sitemap.xml") {
     const users = await selectUsers();
